b254b1450b4664ff3d1304ea776178c28e6314ef
vicpyr <153008819+Rizsaurav@users.noreply.github.com>
2025-02-06 00:10:59 -0600

    Update Home2.js
    twitter username

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -62,7 +62,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Sauravvv"
+                  href="https://twitter.com/Sauravvvvvv"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
